2dd4fd0b51aba5d0c2d831987013b1a0f210ab13
Theodore Konukhov <komolov.f@gmail.com>
2015-05-02 05:34:07 +0300

    t # This is a combination of 2 commits.
    readme
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    oops
    
    screensaver

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pong
+
+I wanted to try Reagent + ClojureScript so I decided to make a Pong game.
+
+![](http://zippy.gfycat.com/BouncyFilthyChihuahua.gif)
+
+Or... a perfect screensaver!
+
+![](https://flavorwire.files.wordpress.com/2011/03/the-office2.jpg)
+
+## Running
+
++ You need to have Leiningen installed in your system.
++ Install dependencies and run the app.
+```
+  lein deps
+  lein figwheel
+```
++ Go to http://localhost:3449
--- src/cljs/pong/ball.cljs
+++ src/cljs/pong/ball.cljs
@@ -10,27 +10,27 @@
   (clojure.core/atom {:x 0
                       :y 0}))
 
-;; speed = x-speed + y-speed
-;; speed^2 = x-speed^2 + y-speed^2
-;; speed
-
 (def base-speed 15)
 (def sqrt (.-sqrt js/Math))
 
 (defn mirror!
+  "Change velocity's sign"
   [key]
   (swap! speed update key -))
 
 (defn init-speed!
+  "Init random x- and y- components:
+   2*base-speed^2 = speed-x^2 + speed-y^2"
   []
   (let [sign (fn [] (rand-nth [-1 1]))
         x (* (sign) (rand base-speed))
         y (* (sign) (sqrt (- (* base-speed 2) (* x x))))]
     (reset! speed {:x x :y y})))
 
-(init-speed!)
+(init-speed!) ;; TODO: get rid of this
 
 (defn delta
+  "Calculate dx or dy. c-key could be :x or :y"
   [c-key binding]
   (let [pos (c-key @position)
         speed (c-key @speed)
@@ -41,6 +41,7 @@
     speed))
 
 (defn move!
+  "Move ball."
   []
   (let [{:keys [:board-width :board-height]} c/dims
         dx (delta :x board-width)
@@ -49,6 +50,7 @@
     (swap! position update :y (fn [i] (+ dy i)))))
 
 (defn ball
+  "Ball's React component"
   []
   (let [y (@position :y)
         x (@position :x)]
